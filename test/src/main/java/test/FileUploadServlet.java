e02f70aa00857e05433a95ddb31ab3b218bab4ee
XuAnJXUFE <1144202622@qq.com>
2020-06-01 13:29:36 +0800

    jk

---
+++ test/src/main/java/test/FileUploadServlet.java
@@ -0,0 +1,31 @@
+package test;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FileUploadServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	public FileUploadServlet() {
+        super();
+    }
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+		
+		
+		
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp){
+		 
+		 
+	}
+	 
+	 
+	
+	
+	
+}
